fc39a2762161a7366a26b3d2a3172e2e11201f71
da3dsoul <da3dsoul@gmail.com>
2019-03-06 01:38:29 -0500

    Bump version on routing

--- nakamoriplugin.py
+++ nakamoriplugin.py
@@ -1,15 +1,17 @@
 import json
-import sys
 
-import routing
-from lib import kodi_utils
-from nakamori_utils.globalvars import *
-from kodi_models.kodi_models import DirectoryListing
 import debug
+import routing
 from error_handler import try_function, show_messages, ErrorPriority
+from kodi_models.kodi_models import DirectoryListing
+from lib import kodi_utils
 from nakamori_utils import nakamoritools as nt
+from nakamori_utils.globalvars import *
 
+
+plugin_localize = plugin_addon.getLocalizedString
 routing_plugin = routing.Plugin('plugin://plugin.video.nakamori')
+url_for = routing_plugin.url_for
 
 
 @routing_plugin.route('/')
@@ -20,21 +22,21 @@ def main():
 
 
 @routing_plugin.route('/menu/main')
-def show_main_menu():
-    show_filter_menu(0)
-
-
-@routing_plugin.route('/menu/filter/<filter_id>')
 @try_function(ErrorPriority.BLOCKING)
-def show_filter_menu(filter_id):
+def show_main_menu():
     from shoko_models.v2 import Filter
-    filter = Filter(filter_id, build_full_object=True, get_children=True)
+    filter = Filter(0, build_full_object=True, get_children=True)
     dir = DirectoryListing('tvshows')
     items = []
     # this just throws an error. It's for testing and should be removed later.
     # filter = items[1]
     for item in filter:
         items.append(item)
+    # apply settings for main menu
+    items[:] = [x for x in items if not is_main_menu_item_enabled(x)]
+
+    add_extra_main_menu_items(items)
+
     # sort the filters
     try:
         # if they are all zero, preserve server sorting
@@ -44,9 +46,56 @@ def show_filter_menu(filter_id):
         pass
     for item in items:
         dir.append(item.get_listitem())
-    del dir
 
 
+def is_main_menu_item_enabled(item):
+    """
+
+    :param item:
+    :type item: Filter
+    :return:
+    """
+    # This only has one at the moment, but we may add the ability for more later
+    if item.name == 'Unsorted Files' and not plugin_addon.getSetting('show_unsort') == 'true':
+        return False
+
+
+def add_extra_main_menu_items(items):
+    """
+    Add items like search, calendar, etc
+    :param items:
+    :return:
+    """
+    from shoko_models.v2 import CustomItem
+    # { 'Airing Today': 0, 'Calendar': 1, 'Seasons': 2, 'Years': 3, 'Tags': 4, 'Unsort': 5, 'Settings': 7,
+    # 'Shoko Menu': 8, 'Search': 9 }
+    if plugin_addon.getSetting('show_airing_today') == 'true':
+        items.append(CustomItem(plugin_localize(30223), 'airing.png', url_for(show_airing_today_menu), 1))
+
+    if plugin_addon.getSetting('show_calendar') == 'true':
+        items.append(CustomItem(plugin_localize(30222), 'calendar.png', url_for(show_calendar_menu), 2))
+
+    if plugin_addon.getSetting('show_settings') == 'true':
+        items.append(CustomItem(plugin_localize(30107), 'settings.png', url_for(show_settings_dialog), 7))
+
+    if plugin_addon.getSetting('show_shoko') == 'true':
+        items.append(CustomItem(plugin_localize(30115), 'settings.png', url_for(show_shoko_menu), 8))
+
+    if plugin_addon.getSetting('show_search') == 'true':
+        items.append(CustomItem(plugin_localize(30221), 'search.png', url_for(show_search_menu), 9))
+
+
+@routing_plugin.route('/menu/filter/byid/<filter_id>')
+@try_function(ErrorPriority.BLOCKING)
+def show_filter_menu(filter_id):
+    from shoko_models.v2 import Filter
+    f = Filter(filter_id, build_full_object=True, get_children=True)
+    d = DirectoryListing('tvshows')
+    for item in f:
+        d.append(item.get_listitem())
+
+
+# do not make urls like /menu/filter/<filter_id> and /menu/filter/unsorted, as they will conflict randomly
 @routing_plugin.route('/menu/filter/unsorted')
 @try_function(ErrorPriority.BLOCKING)
 def show_unsorted_menu():
@@ -105,17 +154,50 @@ def int_add_episode(item, dir):
     dir.append(item.get_listitem(), False)
 
 
+@routing_plugin.route('/menu/airing_today')
+@try_function(ErrorPriority.BLOCKING)
+def show_airing_today_menu():
+    pass
+
+
+@routing_plugin.route('/menu/calendar')
+@try_function(ErrorPriority.BLOCKING)
+def show_calendar_menu():
+    pass
+
+
+@routing_plugin.route('/menu/search')
+@try_function(ErrorPriority.BLOCKING)
+def show_search_menu():
+    pass
+
+
+@routing_plugin.route('/menu/shoko')
+@try_function(ErrorPriority.BLOCKING)
+def show_shoko_menu():
+    pass
+
+
+@routing_plugin.route('/dialog/settings')
+@try_function(ErrorPriority.BLOCKING)
+def show_settings_dialog():
+    pass
+
+
 @routing_plugin.route('/dialog/wizard')
+@try_function(ErrorPriority.BLOCKING)
 def show_wizard_dialog():
     pass
 
 
 @routing_plugin.route('/dialog/vote_series/<series_id>')
+@try_function(ErrorPriority.BLOCKING)
 def show_series_vote_dialog(series_id):
     pass
 
 
 @routing_plugin.route('/dialog/vote_episode/<ep_id>')
+@try_function(ErrorPriority.BLOCKING)
 def show_episode_vote_dialog(ep_id):
     pass
 
@@ -142,11 +224,13 @@ def resume_video(ep_id, file_id):
 
 
 @routing_plugin.route('/series/<series_id>/vote/<value>')
+@try_function(ErrorPriority.BLOCKING)
 def vote_for_series(series_id, value):
     pass
 
 
 @routing_plugin.route('/episode/<ep_id>/vote/<value>')
+@try_function(ErrorPriority.BLOCKING)
 def vote_for_episode(ep_id, value):
     pass
 
@@ -180,7 +264,7 @@ def _main():
     auth, apikey = try_function(ErrorPriority.BLOCKING, '')(nt.valid_user)()
     if not auth:
         raise Exception('Wrong Username or Password, or unable to connect to the server.')
-    try_function(ErrorPriority.BLOCKING)(routing_plugin.run())()
+    try_function(ErrorPriority.BLOCKING)(routing_plugin.run)()
     show_messages()
 
 
